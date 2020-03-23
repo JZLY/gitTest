b9ccc6a1aadc3bc5c311346ffbec039e2fc22c9a
TR <123@qq.com>
2020-03-24 01:06:25 +0800

    又一次修改

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,6 +2,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一次修改");
         System.out.println("第二次提交");
-        System.out.println("第三次修改");
     }
 }
